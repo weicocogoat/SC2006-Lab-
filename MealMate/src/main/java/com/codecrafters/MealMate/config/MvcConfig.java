ca156d760bf3ec6707bcef043efd7a588f1f71c4
bchewzy <bchewzy@gmail.com>
2023-03-06 23:51:52 +0800

    add mvcconfig for page navigation
    + empty files to setup css and js folders

---
+++ MealMate/src/main/java/com/codecrafters/MealMate/config/MvcConfig.java
@@ -0,0 +1,22 @@
+package com.codecrafters.MealMate.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/images")
+                .addResourceLocations("classpath:/static/","classpath:/images/")
+                .setCachePeriod(0);
+    }
+}
