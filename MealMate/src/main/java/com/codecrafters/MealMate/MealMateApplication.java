a6acbf7257f4eb5ce6e0ae5f32f9e942387a7bb6
bchewzy <bchewzy@gmail.com>
2023-03-06 22:43:00 +0800

    added sb project folder

---
+++ MealMate/src/main/java/com/codecrafters/MealMate/MealMateApplication.java
@@ -0,0 +1,13 @@
+package com.codecrafters.MealMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealMateApplication.class, args);
+	}
+
+}
---
+++ MealMate/src/test/java/com/codecrafters/MealMate/MealMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecrafters.MealMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
