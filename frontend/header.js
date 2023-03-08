0c70453222ac068fb7630fda89d33223f5ce05c4
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2023-03-08 22:27:44 +0800

    Add files via upload

---
+++ frontend/header.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    document.querySelector("#home").onclick = function() {
+        //temp code. will be replaced later
+        console.log("Home button pressed.")
+    }
+
+    document.querySelector("#recipe").onclick = function() {
+        //temp code. will be replaced later
+        console.log("Recipe button pressed.")
+    }
+
+    document.querySelector("#storeFinder").onclick = function() {
+        //temp code. will be replaced later
+        console.log("Store Finder button pressed.")
+    }
+
+    document.querySelector("#aboutUs").onclick = function() {
+        //temp code. will be replaced later
+        console.log("About us button pressed.")
+    }
+
+    document.querySelector("#signin").onclick = function() {
+        //temp code. will be replaced later
+        console.log("Sign in button pressed.")
+    }
+});
+
