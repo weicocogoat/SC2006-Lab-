159cf3f140ea30d31f723a4bc49a0edbd67cb610
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2023-03-14 14:23:35 +0800

    Add files via upload

---
+++ frontend/transition.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    document.querySelector('#home_button').onclick = function() {
+        window.location.href="homescreen(signed in).html";
+    }
+
+});
