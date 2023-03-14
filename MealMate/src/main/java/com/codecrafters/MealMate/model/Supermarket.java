710d4208d420672ebdda8aff65a757333183c2cd
iamshlomo <shlomoneken@gmail.com>
2023-03-14 13:48:56 +0800

    Update Supermarket.java

--- MealMate/src/main/java/com/codecrafters/MealMate/model/Supermarket.java
+++ MealMate/src/main/java/com/codecrafters/MealMate/model/Supermarket.java
@@ -25,23 +25,11 @@ public class Supermarket {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Double getLatitude() {
         return latitude;
-    }
-
-    public void setLatitude(Double latitude) {
-        this.latitude = latitude;
     }
 
     public Double getLongitude() {
         return longitude;
     }
-
-    public void setLongitude(Double longitude) {
-        this.longitude = longitude;
-    }    
 }
