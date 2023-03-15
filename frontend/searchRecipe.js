7bf470ba138454e7702853edbd81f1809c01d416
bchewzy <bchewzy@gmail.com>
2023-03-16 00:20:53 +0800

    test fetch api to retrieve recipes
    to add diet type and meal type attribute to recipes

--- frontend/searchRecipe.js
+++ frontend/searchRecipe.js
@@ -7,4 +7,39 @@ document.addEventListener('DOMContentLoaded', function () {
     slider.oninput = function () {
         output.innerHTML = this.value;
     }
+
+    getRecipes();
 });
+
+// Get All Recipes from DB
+function getRecipes() {
+	console.log("Retrieving receipes from database...");
+
+	fetch("http://localhost:8080/recipes/all")
+		.then((resp) => resp.json())
+		.then(function(response) {
+
+			console.log(response);
+
+			response.forEach(function (item, index) {
+                const recipeObj = {
+                    author: item.author,
+                    calories: item.calories,
+                    dateCreated: item.dateCreated,
+                    descrtipion: item.description,
+                    ingredients: item.ingredients,
+                    numOfBookmarks: item.numofBookmarks,
+                    portions: item.portions,
+                    preparationTime: item.preparationTime,
+                    steps: item.steps,
+                    title: title
+               };
+
+               console.log(recipeObj);
+          });
+
+		}).catch(function(error) {
+			console.log("Error retrieving from database");
+			console.log(error);
+		});
+}
