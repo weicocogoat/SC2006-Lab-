ae6a5ec71477e31416e999d727a6cc9b53a594f4
bchewzy <bchewzy@gmail.com>
2023-03-18 20:33:37 +0800

    add thymeleaf for fragments
    other notes:
    - added navbar.html fragment
    - combine homescreen.css to main.css

--- MealMate/src/main/java/com/codecrafters/MealMate/config/MvcConfig.java
+++ MealMate/src/main/java/com/codecrafters/MealMate/config/MvcConfig.java
@@ -10,10 +10,11 @@ public class MvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/").setViewName("forward:/index.html");
-        registry.addViewController("/login").setViewName("forward:/login.html");
-        registry.addViewController("/register").setViewName("forward:/register.html");
-        registry.addViewController("/recipes").setViewName("forward:/searchRecipe.html");
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/recipes").setViewName("searchRecipe");
+        registry.addViewController("/recipes/{id}").setViewName("recipeDetails");
     }
 
     @Override
