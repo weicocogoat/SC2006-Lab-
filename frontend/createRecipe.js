643f8c3c9cbf523d682d48451f7c632a1891ac5a
bchewzy <bchewzy@gmail.com>
2023-03-20 15:14:03 +0800

    continue createRecipe

--- frontend/createRecipe.js
+++ frontend/createRecipe.js
@@ -7,54 +7,74 @@ function searchIngredient() {
 		// Show some error msg here
 		console.log("pls enter");
 	} else {
-		fetch("https://api.spoonacular.com/food/ingredients/autocomplete?query="+ ingredient +"&number=5&apiKey=46e36064541946a6bf3c8f20c84f188a")
+		// Reset Search Results
+		const ingredientsCont = document.getElementById("ingredientList");
+		ingredientsCont.innerHTML = "";
+
+		fetch("https://api.spoonacular.com/food/ingredients/autocomplete?query="+ ingredient +"&metaInformation=true&number=2&apiKey=46e36064541946a6bf3c8f20c84f188a")
 			.then((resp) => resp.json())
 			.then(function(response) {
 
-				console.log(response);
+				response.forEach(function(item, index) {
+					const ingredientObj = {
+						id: item.id,
+						name: item.name,
+						image: item.image
+					}
+
+					// Display Result
+					displayResults(ingredientObj);
+				});
 
 			}).catch(function(error) {
 				console.log(error);
 			});
 	}
-
-	/*
-	fetch("https://api.spoonacular.com/food/ingredients/autocomplete?apiKey=46e36064541946a6bf3c8f20c84f188a")
-		.then((resp) => resp.json())
-		.then(function(response) {
-
-			//console.log(response);
-
-			response.forEach(function (item, index) {
-                const recipeObj = {
-                	id: item._id,	// currently returning undefined, need to fix
-                    author: item.author,
-                    calories: item.calories,
-                    dateCreated: item.dateCreated,
-                    description: item.description,
-                    ingredients: item.ingredients,
-                    numOfBookmarks: item.numOfBookmarks,
-                    portions: item.portions,
-                    preparationTime: item.preparationTime,
-                    steps: item.steps,
-                    title: item.title
-               };
-
-               // Store inside global array
-               listOfRecipes.push(recipeObj);
-          	});
-
-          	// Display Recipes
-          	displayRecipes();
-
-		}).catch(function(error) {
-			console.log(error);
-		});
-		*/
 }
 
-function addIngredient(item) {
+function displayResults(ingredient) {
+	console.log(ingredient);
 
+	const ingredientsCont = document.getElementById("ingredientList");
+
+	const newItem = 
+	`<li>
+		<img src="https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}">
+		${ingredient.name}
+		<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#exampleModal" data-ingredientId="${ingredient.id}" onclick="displayModalDetails(${ingredient})"><i class="fa-solid fa-plus"></i></button>
+	</li>`;
+
+	ingredientList.innerHTML += newItem;
+}
+
+function displayModalDetails(ingredient) {
+	// Load Details
+	const name = document.getElementById("ingredientName");
+	name.innerHTML = ingredient.name;
+}
+
+function calculateNutrition(item) {
+	console.log(item); 
+
+	// Get Individual Ingredient Details (i.e. Calories)
+	fetch("https://api.spoonacular.com/food/ingredients/autocomplete?query="+ ingredient +"&metaInformation=true&number=2&apiKey=46e36064541946a6bf3c8f20c84f188a")
+			.then((resp) => resp.json())
+			.then(function(response) {
+
+				response.forEach(function(item, index) {
+					const ingredientObj = {
+						id: item.id,
+						name: item.name,
+						image: item.image
+					};
+
+					// Display Result
+					getIngredientDetails(ingredientObj);
+				});
+
+			}).catch(function(error) {
+				console.log(error);
+			});
 }
 
 const listOfSteps = [];
