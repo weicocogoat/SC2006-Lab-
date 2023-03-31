6fadaa241af9c289caea6fa29a5fe57e3c84daa6
bchewzy <bchewzy@gmail.com>
2023-03-31 21:05:19 +0800

    fix

--- MealMate/src/main/java/com/codecrafters/MealMate/controller/MealController.java
+++ MealMate/src/main/java/com/codecrafters/MealMate/controller/MealController.java
@@ -1,6 +1,7 @@
 package com.codecrafters.MealMate.controller;
 
 import com.codecrafters.MealMate.model.Meal;
+import com.codecrafters.MealMate.repository.MealRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +16,7 @@ public class MealController {
     private MealRepository mealRepo;
 
     @CrossOrigin
-    @GetMApping("/all")
+    @GetMapping("/all")
     public List<Meal> getMeal()
     {
         return mealRepo.findAll();
@@ -30,9 +31,11 @@ public class MealController {
 
     @CrossOrigin
     @PostMapping("/save")
-    public void saveMeal(@RequestBody Meal recipe)
+    public void saveMeal(@RequestBody Meal recipe) {
+        mealRepo.save(recipe);
+    }
 
-    @CrossPrigin
+    @CrossOrigin
     @DeleteMapping("delete/{id}")
     public void deleteMeal(@PathVariable String id)
     {
--- MealMate/src/main/java/com/codecrafters/MealMate/repository/MealRepository.java
+++ MealMate/src/main/java/com/codecrafters/MealMate/repository/MealRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository; 
 
 @Repository
-public interface MealRepository extends MangoRepository<Recipe, String> {
+public interface MealRepository extends MongoRepository<Meal, String> {
     
 }
