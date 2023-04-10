2e623c30ebd6ffd2316393cea519f66527160aad
weicocogoat <97502018+weicocogoat@users.noreply.github.com>
2023-04-10 11:06:08 +0800

    test

---
+++ MealMate/src/main/resources/static/js/createRecipe.js
@@ -0,0 +1,403 @@
+$(document).ready(function() {
+	// Search Ingredient on Enter Keypress (if search input in focus)
+	$('#ingredientInput').on('keyup', function(e) {
+		if(e.key === "Enter" || e.key === 13) {
+			searchIngredient();
+		}
+	});
+
+	// Add Step on Enter Keypress (if textarea in focus)
+	$('#stepInput').on('keyup', function(e) {
+		if(e.key === "Enter" || e.key === 13) {
+			addStep();
+		}
+	});
+
+});
+
+let imgDataURL;
+function getImage(input) {
+
+	const img = input.files[0];
+
+	const fileReader = new FileReader();
+
+	fileReader.onload = function() {
+		console.log("Result", fileReader.result);
+		imgDataURL = fileReader.result;
+	};
+
+	/*
+	fileReader.onloadend = function() {
+		console.log(imgDataURL);
+	};
+	*/
+
+	fileReader.readAsDataURL(img);
+}
+
+function searchIngredient() {
+	flag = 0;
+	const ingredient = document.getElementById("ingredientInput").value.trim();
+
+	if(ingredient.length == 0) {
+		// Show some error msg here
+		//console.log("pls enter");
+		toastr.error('Please enter a valid name.');
+		
+	} else {
+		fetch("https://api.spoonacular.com/food/ingredients/autocomplete?query="+ ingredient +"&metaInformation=true&number=2&apiKey=46e36064541946a6bf3c8f20c84f188a")
+			.then((resp) => resp.json())
+			.then(function(response) {
+				
+				// Reset search results first
+				const resultsCont = document.getElementById("searchResults");
+				resultsCont.innerHTML = "";
+
+				response.forEach(function(item, index) {
+					const ingredientObj = {
+						id: item.id,
+						name: item.name,
+						image: item.image
+					}
+
+					// Display Result
+					displayResults(ingredientObj);
+					flag = 1;
+				});
+
+				if (flag == 0){
+					toastr.error("No results found");
+				}
+
+			}).catch(function(error) {
+				toastr.error("An error occurred, please try again!", "Failed to Search Ingredient.");
+				console.log(error);
+			});
+	}
+}
+
+// Display Results based on Search Input
+function displayResults(ingredient) {
+	// Once response returns, hide spinner
+	//searchSpinner.classList.remove("d-none");
+
+	const resultsCont = document.getElementById("searchResults");
+
+	const item = {
+		id: ingredient.id,
+		name: ingredient.name,
+		image: ingredient.image
+	};
+
+	console.log(item);
+
+	var ingredientString = JSON.stringify(ingredient);
+	ingredientString = ingredientString.replaceAll('"', "'");
+	
+	const newItem = 
+	`<li>
+		<img src="https://spoonacular.com/cdn/ingredients_100x100/${ingredient.image}">
+		${ingredient.name}
+		<button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#ingredientModal" data-ingredientId="${ingredient.id}" onclick="displayModalDetails(${ingredientString})"><i class="fa-solid fa-plus"></i></button>
+	</li>`;
+
+	resultsCont.innerHTML += newItem;
+}
+
+let currIngredient = {};
+let nutrition;
+let calories = [];
+
+// Display Modal Details of Ingredient to Add
+function displayModalDetails(ingredient) {
+	console.log("Fetching details of ID " + ingredient.id);
+
+	// Get Individual Ingredient Details (i.e. Calories)
+	fetch("https://api.spoonacular.com/food/ingredients/"+ ingredient.id +"/information?apiKey=46e36064541946a6bf3c8f20c84f188a&amount=1")
+			.then((resp) => resp.json())
+			.then(function(response) {
+
+				let weightPerServing = response.nutrition.weightPerServing;		// An array containing amount (e.g. 108) and unit (e.g. g)
+
+				document.getElementById("quantityUnit").innerHTML = weightPerServing.unit;
+
+				nutrition = response.nutrition.nutrients;
+				calories = [];		// stores amount, percentOfDailyNeeds, unit (e.g. kcal)
+
+				for(var i = 0; i < nutrition.length; i++) {
+					if(nutrition[i].name == "Calories") {
+						calories = nutrition[i];
+					}
+				}
+
+				// Get Details of Ingredient as an Object
+				currIngredient = {
+					name: ingredient.name,
+					calories: calories.amount,	// always assumed to be in kcal by default
+					image: ingredient.image,
+					quantity: 1,	// set as 1 by default first
+					quantityUnit: weightPerServing.unit
+				};
+
+			}).catch(function(error) {
+				toastr.error("An error occurred, please try again!", "Failed to Load Details.");
+				console.log(error);
+			});
+
+	const modalTitle = document.getElementById("modalName");
+	modalTitle.innerHTML = ingredient.name;
+}
+ 
+// Calculate Nutrition (i.e. calories) of Ingredient		
+function calculateNutrition() {
+	// Calculate Calories
+	let quantity = document.getElementById("quantity").value;
+
+
+	let totalCalories = Math.round(quantity * calories.amount *100) / 100;
+
+	const nutritionValues = document.getElementById("nutritionValues");
+	nutritionValues.innerHTML = totalCalories + " " + calories.unit;
+
+	// Update Ingredient Object
+	currIngredient.calories = totalCalories;
+	currIngredient.quantity = quantity;
+}
+
+
+let ingredientsList = [];
+
+// Add Ingredient to List
+function addIngredient() {
+	
+	// Hide Modal
+	let ingredientModal = bootstrap.Modal.getInstance(document.querySelector("#ingredientModal"));
+	ingredientModal.hide();
+
+	console.log("Trying to add...");
+	console.log(currIngredient);
+
+	// Add to Global Array
+	ingredientsList.push(currIngredient);
+
+	// Reset Add Ingredient Modal Details
+	$('#quantity').val("");
+	$('#nutritionValues').text("Not calculated yet");
+
+	// Remove empty message if visible
+	if(ingredientsList.length != 0) {
+		document.getElementById("noItemsMsg").classList.remove("d-block");
+		document.getElementById("noItemsMsg").classList.add("d-none");
+	}
+
+	// Display Ingredient (right-side list)
+	const ingredientList = document.getElementById("ingredientList");
+
+	const newItem = 
+	`<li id="${currIngredient.name}ListItem">
+		<img src="https://spoonacular.com/cdn/ingredients_100x100/${currIngredient.image}">
+		${currIngredient.name} - ${currIngredient.calories} kcal (${currIngredient.quantity} ${currIngredient.quantityUnit})
+		<button class="btn" type="button" onclick="removeIngredient('${currIngredient.name}')"><i class="fa-solid fa-xmark"></i></button>
+	</li>`;
+
+	ingredientList.innerHTML += newItem;
+
+	// Show Success Toast
+	toastr.success("Ingredient Successfully Added");
+}
+
+// Remove Ingredient from List
+function removeIngredient(name) {
+	// Remove from Global Array
+	for(var i = 0; i < ingredientsList.length; i++) {
+		if(ingredientsList[i].name == name) {
+			ingredientsList.pop(ingredientsList[i]);
+		}
+	}
+
+	// Remove from View
+	document.getElementById(name + "ListItem").remove();
+
+	// Display empty list msg if no items
+	if(ingredientsList.length == 0) {
+		document.getElementById("noItemsMsg").classList.remove("d-none");
+		document.getElementById("noItemsMsg").classList.add("d-block");
+	}
+}
+
+let listOfSteps = [];		// Global Array of Steps
+
+// Add Step to List
+function addStep() {
+	const stepInput = document.getElementById("stepInput").value;
+	const stepsList = document.getElementById("stepsList");
+
+	let stepNum = listOfSteps.length + 1;
+
+	let newStep = {
+		stepNum: stepNum,
+		stepInstruction: stepInput
+	};
+
+	listOfSteps.push(newStep);
+
+	/* original view with step number
+	const stepView = `
+		<div id="step${newStep.stepNum}Cont" class="recipeStepBox mb-3">
+	        <h6 class="mb-3">
+	            Step ${newStep.stepNum}
+	            <button class="btn rounded-circle float-end" type="button" onclick="removeStep(${newStep.stepNum})"><i class="fa-solid fa-trash"></i></button>
+	        </h6>
+
+	        <p>${newStep.stepInstruction}</p>
+	    </div>
+	`;
+	*/
+
+	const stepView = `
+		<div id="step${newStep.stepNum}Cont" class="recipeStepBox mb-3">
+	        <p>
+	        	${newStep.stepInstruction}
+	        	<button class="btn rounded-circle float-end" type="button" onclick="removeStep(${newStep.stepNum})"><i class="fa-solid fa-trash"></i></button>
+        	</p>
+	    </div>
+	`;
+
+	stepsList.innerHTML += stepView;
+
+	// Reset input value
+	document.getElementById("stepInput").value = "";
+}
+
+// Remove Step from List
+function removeStep(stepNum) {
+	// Remove from Global Array
+	for(var i = 0; i < listOfSteps.length; i++) {
+		if(listOfSteps[i].stepNum == stepNum)
+			listOfSteps.pop(listOfSteps[i]);
+	}
+
+	// Remove from View
+	document.getElementById("step" + stepNum + "Cont").remove();
+}
+
+// Create Recipe
+function createRecipe() {
+	// Get General Information
+	const recipeTitle = document.getElementById("title").value;
+	const servingSize = document.getElementById("servingSize").value;
+	const prepTime = document.getElementById("prepTime").value;
+
+	if (recipeTitle === ""){
+		toastr.error("Please input a Recipe Title.");
+		return;
+	}
+	if (servingSize === ""  || parseInt(servingSize) < 1){
+		toastr.error("Please input a positive Serving Size.");
+		return;
+	}
+	if (prepTime === ""  || parseInt(prepTime) < 1){
+		toastr.error("Please input a positive Preparation Time.");
+		return;
+	}
+	console.log(imgDataURL);
+	if (imgDataURL === undefined){
+		toastr.error("Please add image.");
+		return;
+	}
+
+	let dietType = [];
+	$('input[name="dietType"]:checked').each(function() {
+		dietType.push(this.value);
+	});
+
+	const description = document.getElementById("desc").value;
+	if (description === ""){
+		toastr.error("Please input a Description.");
+		return;
+	}
+
+	if (ingredientsList.length == 0){
+		toastr.error("Please add the ingredients.");
+		return;
+	}
+
+	if(listOfSteps.length == 0){
+		toastr.error("Please add the list of steps.");
+		return;
+	}
+
+	// Calculate Total Calories for Entire Recipe
+	let totalCalories = 0;
+	for(var i = 0; i < ingredientsList.length; i++) {
+		totalCalories += ingredientsList[i].calories;
+	}
+
+	const newRecipe = {
+		title: recipeTitle,
+		author: localStorage.getItem("id"),	// save userId for author
+		description: description,
+		dateCreated: new Date().toISOString(),
+		dietType: dietType,
+		numOfBookmarks: 0,
+		prepTime: prepTime,
+		servingSize: servingSize,
+		calories: totalCalories,
+		ingredients: ingredientsList,
+		steps: listOfSteps,
+		image: imgDataURL
+	};
+
+	const testOutput = {
+		title: recipeTitle,
+		author: localStorage.getItem("id"),	// save userId for author
+		description: description,
+		dateCreated: new Date().toISOString(),
+		dietType: dietType,
+		numOfBookmarks: 0,
+		prepTime: prepTime,
+		servingSize: servingSize,
+		calories: totalCalories,
+		ingredients: ingredientsList,
+		steps: listOfSteps
+	};
+
+	console.log(testOutput);
+
+	fetch('http://localhost:8080/api/recipes/save', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newRecipe)
+        })
+        .then(response => {
+        	//response.json()
+        })
+        .then(data => {
+        	toastr.success("Recipe successfully created.", "Recipe Created!");
+        	resetForm();
+        })
+        .catch((error) => {
+        	toastr.error("An error occurred, please try again!", "Failed to Create Recipe.");
+            console.error('Error:', error);
+        });
+}
+
+// Reset Create Recipe Form (Input fields and lists)
+function resetForm() {
+	// Reset Form Fields
+	$('#newRecipeForm').trigger('reset');
+
+	// Reset Ingredients List
+	ingredientsList = [];
+	$('#noItemsMsg').toggleClass('d-none');
+	$('#noItemsMsg').toggleClass('d-block');
+	$('#ingredientList').html("");
+
+
+	// Reset List of Steps
+	listOfSteps = [];
+	$('#stepsList').html("");
+}
---
+++ MealMate/src/main/resources/static/js/searchRecipe.js
@@ -0,0 +1,309 @@
+// Array that will hold all recipe names retrieved from DB
+var listOfRecipeName = [];
+
+document.addEventListener('DOMContentLoaded', function () {
+    var slider = document.getElementById("myRange");
+    var output = document.getElementById("val");
+    output.innerHTML = slider.value; // Display the default slider value
+
+    // Update the current slider value (each time you drag the slider handle)
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+    }
+
+    // Retrieve data from DB
+    getRecipes();
+	autocomplete(document.getElementById("recipeInput"), listOfRecipeName);
+
+});
+
+// Array that will hold all recipes retrieved from DB
+var listOfRecipes = [];
+var dietTypeFilter = [];
+var cal;
+
+// Get All Recipes from DB
+function getRecipes() {
+	console.log("Retrieving receipes from database...");
+
+	listOfRecipes = [];
+	listOfRecipeName = [];
+	dietTypeFilter = [];
+	var flag2 = 0;
+
+	if (document.getElementById("dairyFree").checked){
+		dietTypeFilter.push("dairy free");
+	}
+	if (document.getElementById("glutenFree").checked){
+		dietTypeFilter.push("gluten free");
+	}
+	if (document.getElementById("vegan").checked){
+		dietTypeFilter.push("vegan");
+	}
+	if (document.getElementById("vegetarian").checked){
+		dietTypeFilter.push("vegetarian");
+	}
+
+	//check if all boxes are unchecked
+	for (i= 0; i < 4; i++){
+		if (dietTypeFilter == true){
+			flag2 = 1;
+		}
+	}
+
+
+	cal = parseInt(document.getElementById("myRange").value);
+
+	fetch("http://localhost:8080/api/recipes/all")
+		.then((resp) => resp.json())
+		.then(function(response) {
+			response.forEach(function (item, index) {
+                const recipeObj = {
+                	id: item.id,
+                	title: item.title,
+                    author: item.author,
+                    image: item.image,
+                    description: item.description,
+                    dateCreated: item.dateCreated,
+                    dietType: item.dietType,
+                    numOfBookmarks: item.numOfBookmarks,
+                    prepTime: item.prepTime,
+                    servingSize: item.servingSize,
+                    calories: item.calories,
+                    ingredients: item.ingredients,
+                    steps: item.steps
+               };
+
+               // Store inside global array
+			   if (flag2 == 0){
+					listOfRecipes.push(recipeObj);
+					listOfRecipeName.push(recipeObj["title"]);
+			   }
+			   else{
+					for (var i = 0; i < recipeObj["dietType"].length; i++){
+						if (dietTypeFilter.includes(recipeObj["dietType"][i])){
+							listOfRecipes.push(recipeObj);
+							listOfRecipeName.push(recipeObj["title"]);
+							break;
+						}
+					}
+			   }
+
+          	});
+
+			for (var i = listOfRecipes.length - 1; i >= 0; i--){
+				//console.log(listOfRecipes[i].calories);
+				//console.log(cal);
+				if (listOfRecipes[i].calories > cal){
+					listOfRecipes.splice(i, 1);
+					listOfRecipeName.splice(i, 1);
+				}
+		    }
+
+          	// Display Recipes
+          	displayRecipes();
+
+		}).catch(function(error) {
+			console.log(error);
+		});
+}
+
+// Recipe Card HTML View Template
+const recipeView = (recipe) => 
+	`
+	    <div class="recipeCard card shadow-sm mb-4" data-aos="fade-up">
+	        <div class="row no-gutters">
+	            <div class="col-md-3">
+	            	<div class="recipeCardImg">
+	            		<img src="${recipe.image}" class="card-img">
+	            		<span class="recipeCalories">${recipe.calories} kcal</span>
+	            	</div>
+	                
+	            </div>
+	            <div class="col-md-9">
+	                <div class="card-body">
+	                	${generateBadges(recipe)}
+	                    <div id="firstLine">
+	                        <h5 id="title">${recipe.title}</h5>
+	                        <h6 id="star-rating">${recipe.numOfBookmarks} <i class="fa-solid fa-star"></i></h6>
+	                    </div>
+	                    <div id="secondLine" class="text-muted">
+	                        <p id="servingSize">${recipe.servingSize} Servings</p> 
+	                        <p id="duration">${recipe.prepTime} Hours</p>
+	                    </div>
+	                    <p class="card-text">${recipe.description}</p>
+	                    <div class="d-flex justify-content-between align-items-center">
+	                        <a class="textLink stretched-link" href="recipes/${recipe.id}"><h6>Read More <i
+	                            class="fa-solid fa-arrow-right"></i></h6></a>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	`
+;
+
+function generateBadges(recipe) {
+	let badgesView = "";
+
+	for(var i = 0; i < recipe.dietType.length; i++) {
+		let dietType = recipe.dietType[i];
+
+		badgesView += "<span class='badge bg-secondary badgeItem mb-2'>" + dietType + "</span>";
+	}
+
+	return badgesView;
+}
+
+// Render and Display Recipes in View
+function displayRecipes() {
+	const recipeViewList = [];	// Array that will hold all individual HTML views to generate of each recipe
+
+	for(var i=0; i < listOfRecipes.length; i++) {
+		const recipe = listOfRecipes[i];
+		const itemViewHTML = recipeView(recipe);
+
+		recipeViewList.push(itemViewHTML);
+	}
+
+	// Combine all the recipe cards together into a string
+	const recipeViewHTML = recipeViewList.join("\n");
+
+	// Insert into Recipe Container
+	const recipeContainer = document.getElementById("recipeContainer");
+	recipeContainer.innerHTML = recipeViewHTML;
+}
+
+function autocomplete(inp, arr) {
+	/*the autocomplete function takes two arguments,
+	the text field element and an array of possible autocompleted values:*/
+	var currentFocus;
+	/*execute a function when someone writes in the text field:*/
+	inp.addEventListener("input", function(e) {
+		var a, b, i, val = this.value;
+		/*close any already open lists of autocompleted values*/
+		closeAllLists();
+		if (!val) { return false;}
+		currentFocus = -1;
+		/*create a DIV element that will contain the items (values):*/
+		a = document.createElement("DIV");
+		a.setAttribute("id", this.id + "autocomplete-list");
+		a.setAttribute("class", "autocomplete-items");
+		/*append the DIV element as a child of the autocomplete container:*/
+		this.parentNode.appendChild(a);
+		/*for each item in the array...*/
+		for (i = 0; i < arr.length; i++) {
+		  /*check if the item starts with the same letters as the text field value:*/
+		  if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+			/*create a DIV element for each matching element:*/
+			b = document.createElement("DIV");
+			/*make the matching letters bold:*/
+			b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+			b.innerHTML += arr[i].substr(val.length);
+			/*insert a input field that will hold the current array item's value:*/
+			b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+			/*execute a function when someone clicks on the item value (DIV element):*/
+				b.addEventListener("click", function(e) {
+				/*insert the value for the autocomplete text field:*/
+				inp.value = this.getElementsByTagName("input")[0].value;
+				/*close the list of autocompleted values,
+				(or any other open lists of autocompleted values:*/
+				closeAllLists();
+			});
+			a.appendChild(b);
+		  }
+		}
+	});
+	/*execute a function presses a key on the keyboard:*/
+	inp.addEventListener("keydown", function(e) {
+		var x = document.getElementById(this.id + "autocomplete-list");
+		if (x) x = x.getElementsByTagName("div");
+		if (e.keyCode == 40) {
+		  /*If the arrow DOWN key is pressed,
+		  increase the currentFocus variable:*/
+		  currentFocus++;
+		  /*and and make the current item more visible:*/
+		  addActive(x);
+		} else if (e.keyCode == 38) { //up
+		  /*If the arrow UP key is pressed,
+		  decrease the currentFocus variable:*/
+		  currentFocus--;
+		  /*and and make the current item more visible:*/
+		  addActive(x);
+		} else if (e.keyCode == 13) {
+		  /*If the ENTER key is pressed, prevent the form from being submitted,*/
+		  e.preventDefault();
+		  if (currentFocus > -1) {
+			/*and simulate a click on the "active" item:*/
+			if (x) x[currentFocus].click();
+		  }
+		}
+	});
+	function addActive(x) {
+	  /*a function to classify an item as "active":*/
+	  if (!x) return false;
+	  /*start by removing the "active" class on all items:*/
+	  removeActive(x);
+	  if (currentFocus >= x.length) currentFocus = 0;
+	  if (currentFocus < 0) currentFocus = (x.length - 1);
+	  /*add class "autocomplete-active":*/
+	  x[currentFocus].classList.add("autocomplete-active");
+	}
+	function removeActive(x) {
+	  /*a function to remove the "active" class from all autocomplete items:*/
+	  for (var i = 0; i < x.length; i++) {
+		x[i].classList.remove("autocomplete-active");
+	  }
+	}
+	function closeAllLists(elmnt) {
+	  /*close all autocomplete lists in the document,
+	  except the one passed as an argument:*/
+	  var x = document.getElementsByClassName("autocomplete-items");
+	  for (var i = 0; i < x.length; i++) {
+		if (elmnt != x[i] && elmnt != inp) {
+		x[i].parentNode.removeChild(x[i]);
+	  }
+	}
+  }
+  /*execute a function when someone clicks in the document:*/
+  document.addEventListener("click", function (e) {
+	  closeAllLists(e.target);
+  });
+  }
+
+function searchIngredient(recipeName){
+	console.log(listOfRecipeName);
+
+	if (recipeName === ""){
+		toastr.error("Please input a Recipe Title.");
+		return;
+	}
+
+	if(recipeName.length <= 0) {
+		// If search field is empty
+		getRecipes();
+	} else {
+		var i = 0;
+		for(i ; i < listOfRecipeName.length; i++) {
+			if (listOfRecipeName[i] === recipeName){
+				break;
+			}
+		}
+
+		if(i > listOfRecipeName.length) {
+			// No results found
+			$('#recipeContainer').html("<h6 class='text-center'>No search results found!</h6>");
+		}
+
+		const recipeViewList = [];	// Array that will hold all individual HTML views to generate of each recipe
+		const itemViewHTML = recipeView(listOfRecipes[i]);
+		recipeViewList.push(itemViewHTML);
+
+		// Combine all the recipe cards together into a string
+		const recipeViewHTML = recipeViewList.join("\n");
+
+		// Insert into Recipe Container
+		const recipeContainer = document.getElementById("recipeContainer");
+		recipeContainer.innerHTML = recipeViewHTML;
+	}
+}
